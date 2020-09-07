69ed6718e1d9d2e6d3555f874bce7298b0360143
Lasitha Wijethilaka <maxlasitha@gmail.com>
2020-09-07 07:32:28 +0530

    Update myscript_run.sh

--- myscript_run.sh
+++ myscript_run.sh
@@ -2,9 +2,9 @@
 # This is to set raspberry pi into monitoring mode
 iw phy `iw dev wlan0 info | gawk '/wiphy/ {printf "phy" $2}'` interface add mon0 type monitor
 echo "creating monitor interface"
-sleep 5
+sleep 3
 ifconfig mon0 up
 echo "Activate interface mon0"
-sleep 5
-echo "sucess!"
+sleep 3
+echo "........................sucess........................!"
 bettercap -iface mon0
